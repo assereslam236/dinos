83aeb4766242645dd15443bce01b57952cafc1e7
assereslam236 <167943998+assereslam236@users.noreply.github.com>
2025-07-06 17:12:18 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Simulate boot sequence
-window.addEventListener('load', () => {
-  setTimeout(() => {
-    document.getElementById('boot').classList.add('hidden');
-    document.getElementById('main').classList.remove('hidden');
-  }, 3000); // 3-second boot
-});
